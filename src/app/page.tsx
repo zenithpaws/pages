c82eef00765554942dc89100baf33bf7054ecd2d
zenithpaws <harlan.roberts.jr@icloud.com>
2024-08-30 16:51:59 -0400

    Sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
   return (
     <>
       <Header />
-
       <main className='m-0 flex items-center justify-center p-0'>
         <div className={`${styles.grid} absolute left-0 top-0 -z-50 m-0 h-[40rem] w-full p-0`}>
           <div className='relative size-full'>
