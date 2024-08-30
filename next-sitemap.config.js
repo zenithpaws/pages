384b47219054237250916728440ebeec28a9f64c
zenithpaws <harlan.roberts.jr@icloud.com>
2024-08-30 15:55:39 -0400

    Sync

---
+++ next-sitemap.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    siteUrl: 'https:/zenithpaws.dev', // Replace with your site URL
+    generateRobotsTxt: true,
+    priority: 0.7, // Default priority for all pages
+    changefreq: 'daily', // Default change frequency
+    sitemapSize: 5000, // Default max size of the sitemap
+    exclude: ['/404'], // Exclude certain paths (optional)
+    transform: async (config, path) => {
+        if (path === '/') {
+            return {
+                loc: path,
+                priority: 1.0, // Set priority of main page to highest
+                changefreq: 'daily',
+                lastmod: new Date().toISOString(),
+            };
+        }
+
+        // Default return for all other pages
+        return {
+            loc: path,
+            priority: config.priority,
+            changefreq: config.changefreq,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        };
+    }
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import './globals.css';
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react"
 
 /**
  * @description - The root layout of the application
  * @param {React.ReactNode} children - The children
  * @return {JSX.Element} - The root layout
  */
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
@@ -17,8 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       */}
       <head />
       <body>{children}</body>
-      <Analytics />
-      <SpeedInsights />
     </html>
   );
 }
